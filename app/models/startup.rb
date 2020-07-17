651226fec1912c176794c0f80d0caaee74673e2a
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-17 11:05:29 -0500

    startup

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,37 @@
 class Startup
+    
+    attr_accessor :name,:domain
+    attr_reader :founder
+    attr_writer :pivot
+    @@all=[]
+    @@domain=[]
+    def initialize(name,founder,domain)
+        @name=name
+        @founder=founder
+        @domain=domain
+        @@all<<self
+        @@domain<<domain
+    end
+
+    def pivot(new_domain,new_name)
+        self.domain = new_domain  
+        self.name = new_name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        all_com=Startup.all
+        index = all_com.find_index {|value|
+            value.founder==founder_name
+        }
+        puts all_com[index]
+    end
+
+    def self.domains
+        @@domain.uniq
+    end
 
 end
