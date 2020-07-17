2d7764f708987ac8ed0bd840d0e2334e1fa68563
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-17 11:29:28 -0500

    venture_capitalist

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,24 @@
 class VentureCapitalist
+    attr_accessor :name,:total_worth
 
+    @@all=[]
+    def initialize(name,total_worth)
+        @name=name
+        @total_worth=total_worth
+        @@all<<self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        all_ven=VentureCapitalist.all
+        club = []
+        club = all_ven.find_all {|value|
+            value.total_worth > 1000000000
+        }        
+       club
+    end
 end
+
