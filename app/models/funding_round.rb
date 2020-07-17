f32fd9e4a06a49df001e91338260a470c92e0b0f
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-17 11:55:30 -0500

    funding_round

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,32 @@
+require_relative './startup'
+require_relative './venture_capitalist'
 class FundingRound
-
-end
+    attr_accessor :type,:investment
+     attr_reader :startup,:venture_capitalist
+ 
+     @@all=[]
+ 
+     def initialize(type,investment)
+         @type=type
+         @investment=investment
+         @investment>0
+         @@all << self
+     end
+ 
+     def startup
+         self.startup
+     end
+     def venture_capitalist
+         self.venture_capitalist
+     end
+     def type
+         @type
+     end
+     def investment
+         @investment
+     end
+ 
+     def self.all
+         @@all
+     end
+ end
